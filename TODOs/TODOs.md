8ca459bb588254efd3d9370c22de0f6221e04838
yuanfengli168 <jackieliglobal@gmail.com>
2024-10-11 15:19:18 -0700

    NOBUG: cleaned version

--- TODOs/TODOs.md
+++ TODOs/TODOs.md
@@ -5,6 +5,19 @@
 
 4. (10/10/2024) 
 a) add a finished today tab; (finished 10/11/2024 in 3.5 hours, but scalable, and reusable, more succinct);
+[Learnings]:
+    a)  encapsulation+consice!!! -> each mehtod function do not exceed 30lines of code, excluding comments. 
+    b) decoupling -> utils.js created, very good!
+    c) Readable+maintainable: Finished.returnFinished() has double for loop, (I used filter and some at the begining) but really hard to change in future!
+    d) asked fewer questions to the ChatGPT: 
+        -. only 4 questions: 
+    e) finished new feature and worked in 2.5 hours(excluding summary): 
+        -. 1)finished.js, 2)finished.css, 3)utils.js
+    f) 4 ChatGPT Questions I asked: 
+        1) how to change an object to array? I used `Array.from(obj)`;
+        2) how to get object attribue value? turns out I was correct at concept but wrong for typo.
+        3) parent.appendChild js? learned that naming should add element, after you created element!
+        4) how to create a div element and adding class value at the same time? You have to do it in two steps, 1)create; 2)classList.add()
 
 b) add a % in logs; 
 c) add rules of writing, so writers can use them! e.g. 1.2.3. / (1,2,3a) / .,.
--- js/finished.js
+++ js/finished.js
@@ -12,25 +12,19 @@ class Finished {
         return await getOriginalQuizs();
     }
     
-    // return array of quiz which results contains today's date
+    // return array of all quiz which results contains today's date/passed date
     // TODO: 
     // This naming is bad, advice: getArrayOfFinishedQuizs()
     async returnFinished(date) {
         if (!date) {
             date = this.todayDate;
         }
-
         let arrayOfQuizs = await this.getAllQuizs();
 
-        // method1: 
-        // I prefer this one for better readability and maintainability in the future.
         const result = [];
         for (let quiz of arrayOfQuizs) {
             let results = quiz.results;
             for (let res of Array.from(results)) {
-                // blocker 1: 5min block
-                // let finishedDateAndTime  = typeof res.finishedDateAndTime;  
-                // blocker: why finishedDataTime is undefined? because I typed And which is not in the attribute
                 let finishedDateTime  = res.finishedDateTime;
                 let dateFinished = finishedDateTime.split(" ")[0];
                 if (dateFinished === date) {
@@ -39,10 +33,9 @@ class Finished {
             }
         }
 
-        // Method2: following is fully functional
-        // can use but really hard to read and maintain for future engineers, I would prefer the above way:
-        // arrayOfQuizs = arrayOfQuizs.filter(quiz => Array.from(quiz.results)
-                                        //    .some((element) => element.finishedDateTime.split(" ")[0] === date));
+        // it might still be duplicate:
+        // todo: in version2: think of deduplication
+        // @date: 10/11/2024
         this.arrayOfFinishedQuizs = result;
         return result;
     }
@@ -52,7 +45,6 @@ class Finished {
     async returnFinishedForTodayTaskPage() {
         let arrayOfFinished = this.returnFinished();
         let result = (await arrayOfFinished).filter(quiz => this.shouldRenderForTodayTask(quiz));
-
         return result;
     }
 
@@ -61,26 +53,20 @@ class Finished {
         const todayDate = getTodayDate();
         const quizDate = quiz.date;
         // because result's first item is always the oldest time
+        // TODO: write new util fundion to get all infos, @date:10/11/2024
         const quizLastResult = quiz.results[0];
         const quizLastResultDate = quizLastResult.finishedDateTime.split(" ")[0];
         const diffDays = (new Date(todayDate).getTime() - new Date(quizDate).getTime()) / (1000 * 3600 * 24);
+        
         if ( [1,2,7,14,21,28,56].includes(diffDays) || quizLastResultDate === todayDate) {
                 return true;    
         }
+        
         return false;
     }
 
     // return array of quiz Card in html format in string type
     async createQuizCards(date) {
-        // bug2: I forgot to write await here, so the return will
-        // always return an "" string;
-        // const arrayOfFinished = this.returnFinished(date);
-
-        // correct version: 
-        // do this inside of returnFinished.
-        // const arrayOfFinished = await this.returnFinished(date);
-        // update this.arrayOfFinishedQuizs only once in the entire class:
-        // this.arrayOfFinishedQuizs = arrayOfFinished;
         const arrayOfFinished = await this.returnFinishedForTodayTaskPage();
 
         return createQuizCards(arrayOfFinished);
@@ -100,25 +86,15 @@ class Finished {
         if (parentElement) {
             const childElement = document.createElement('div');
             childElement.classList.add('finished-quiz');
-
-            // // Caveat: only update the class member value 
-            // //         when we are on the page that needs it.
-            // this.arrayOfFinishedQuizs = await this.createQuizCards(date || this.date);
-            // console.log("update: ", this.arrayOfFinishedQuizs);
-
             const quizCards = await this.createQuizCards(date || this.date);
             childElement.innerHTML = quizCards;
 
             parentElement.appendChild(childElement)
 
-
             // quick solution, adding listener here so after the await: 
             this.addEventListenerOfTakeQuiz();
             this.addEventListenerOfShowLogs();
         } 
-        else {
-            return;
-        }
     }
 
     // Caveat: this should be split from the other quiz-items 
@@ -126,18 +102,10 @@ class Finished {
     addEventListenerOfTakeQuiz() {
         let selectorName = '.today-finished .finished-quiz .quiz-item .quiz-cards button.take';
         let quizButton = document.querySelectorAll(selectorName);
-        quizButton.forEach((button, index) => {
-            let idx = index;
-            // bug!!!: any this inside of the function() is pointing 
-            //         to the function it self rather than the class 
-            //        Solution: using arrow funtion
-            // button.addEventListener('click', function () {
-            //     console.log("array: ", this.arrayOfFinishedQuizs);
-            //     renderQuizOfIndex(idx, this.arrayOfFinishedQuizs);
-            // })
 
+        quizButton.forEach((button, index) => {
             button.addEventListener('click', () => {
-                renderQuizOfIndex(idx, this.arrayOfFinishedQuizs);
+                renderQuizOfIndex(index, this.arrayOfFinishedQuizs);
             })
         })
     }
@@ -148,11 +116,11 @@ class Finished {
         let selectorName = '.today-finished .finished-quiz .quiz-item .quiz-cards button.logs';
         let logButton = document.querySelectorAll(selectorName);
 
-        
         logButton.forEach((button, index) => {
             button.addEventListener('click', () => {
                 let parentSelectorName = `.today-finished .finished-quiz .quiz-item .card-logs-${index}`;
                 let parent = document.querySelector(parentSelectorName);
+
                 renderLogsOfIndex(index, this.arrayOfFinishedQuizs, false, parent);
                 button.textContent = `Hide Logs`;
                 button.classList.add("hide");
